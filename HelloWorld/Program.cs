138fbc5bd7872307b68b61750db5222b73c0518d
Tai Yu <a-tayu@microsoft.com>
2019-05-25 20:42:18 +0800

    F

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            // Feature
+            // Feature yyyy
         }
     }
 }
